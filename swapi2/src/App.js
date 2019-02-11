1a715dc8011982b7ae65edf9915693810db1d50e
Anthony Bendas <anthony.bendas@gmail.com>
2019-02-11 07:34:40 -0800

    party size state

--- swapi2/src/App.js
+++ swapi2/src/App.js
@@ -6,12 +6,30 @@ import Roll from './Roll';
 import Settings from './Settings';
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+        partySize: 0
+    }
+  }
+
+  addOne() {
+    this.setState({ partySize: this.state.partySize + 1})
+    console.log('hello');
+  }
+
+  minusOne() {
+    console.log('bye');
+  }
 
   render() {
+    const { partySize } = this.state;
     return (
       <div>
-        <Settings />
+        <h1>Party Size</h1>
+        <button id="plus" onClick={this.addOne}>+1</button>
+        <button id="minus" onClick={this.minusOne}>-1</button>        
+        <p>{partySize}</p>
         <Roll />
         <div>
           <Card />
--- swapi2/src/Settings.js
+++ swapi2/src/Settings.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const Settings = () => {
+const Settings = ({ size }) => {
 
 	return (
 		<div>
 			<h1>Star Wars Party Generator</h1>
 			<h2>Party Settings</h2>
-			+1<input type="submit" />
-			-1<input type="submit" />
-			<p>Party Size</p>
-			<p id="size"></p>
+
 		</div>
 	)
 }
