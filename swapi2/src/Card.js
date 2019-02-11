4f2ac31db7946c9ad39e01bb2d1688bd4eee5870
Anthony Bendas <anthony.bendas@gmail.com>
2019-02-10 18:15:30 -0800

    init project, update node, define Card component

---
+++ swapi2/src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+
+	return (
+		<div>
+			<h1>Character</h1>
+			<p>Name</p>
+			<p>Age</p>
+			<p>Birth Place</p>
+			<p>Home World</p>
+		</div>
+	)
+}
+
+export default Card;
