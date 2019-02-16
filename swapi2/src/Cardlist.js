b2bdf0e2ef25f941e1b735af9da659eb1521d29c
Anthony Bendas <anthony.bendas@gmail.com>
2019-02-15 21:03:28 -0800

    back end logic complete to pull random characters

--- swapi2/src/Cardlist.js
+++ swapi2/src/Cardlist.js
@@ -3,15 +3,39 @@ import Card from './Card';
 
 const Cardlist = ({ party, randCharValue }) => {
 	const charList = [];
-	
+	let times = 0;
+	let randNum = 0;
+	let randNumArray = [];
+
+	function genRand() {
+		return Math.floor(Math.random()*87 + 1)
+	}
+
+	while (times < party) {
+		while(true) {
+			let newRand = genRand()
+			if (randNumArray.includes(newRand) || newRand === 17) {
+				console.log("duplicate")
+			} else {
+				randNumArray[times] = newRand
+				break;
+			}
+		}
+		times++;
+	}
+
+	console.log(randNumArray);
+
 	function generateTeam(size) {
 		for (let i = 0; i < size; i++) {
-			charList[i] = "char"
+			fetch(`https://swapi.co/api/people/${randNumArray[i]}`)
+				.then(response => response.json())
+				.then(char => charList[i] = char)
 		}
 	}
 
-	generateTeam();
-	console.log(randCharValue);
+	generateTeam(party);
+	console.log(charList);
 
 	return (
 		<div>
